f962c148496bfa47433b9daeed67b1b5f99863ec
Theo Penavaire <theo.penavaire@gmail.com>
2019-02-11 22:44:39 -0500

    Add Game scene

---
+++ Audience App/Assets/Scripts/Lobby/LobbyManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyManager : MonoBehaviour
+{
+    private const string _GAME_SCENE = "Game";
+
+    public void OnJoinButtonClick()
+    {
+        SceneManager.LoadSceneAsync(_GAME_SCENE);
+    }
+}
