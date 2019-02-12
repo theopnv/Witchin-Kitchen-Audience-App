3ce73699e8e5249233043e992a8334798be70bbc
Theo Penavaire <theo.penavaire@gmail.com>
2019-02-11 22:28:36 -0500

    Prepare UI for the 2 first screens

---
+++ Audience App/Assets/Scripts/Title Screen/TitleScreenManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    private const string _LOBBY_GAME_SCENE = "Lobby";
+
+    public void OnMainButtonClick()
+    {
+        SceneManager.LoadSceneAsync(_LOBBY_GAME_SCENE);
+    }
+}
