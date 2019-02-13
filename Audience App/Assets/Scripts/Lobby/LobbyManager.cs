d41b996873ac147c05d208ffbffc995e0afea8ac
Theo Penavaire <theo.penavaire@gmail.com>
2019-02-13 15:58:41 -0500

    Add Newtonsoft Json + Implement first method of the protocol

--- Audience App/Assets/Scripts/Lobby/LobbyManager.cs
+++ Audience App/Assets/Scripts/Lobby/LobbyManager.cs
@@ -1,14 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using SocketIO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Newtonsoft.Json;
 
-public class LobbyManager : MonoBehaviour
+namespace audience.lobby
 {
-    private const string _GAME_SCENE = "Game";
 
-    public void OnJoinButtonClick()
+    public class LobbyGames
     {
-        SceneManager.LoadSceneAsync(_GAME_SCENE);
+        public List<string> Games;
     }
+
+    public class LobbyManager : MonoBehaviour
+    {
+        private const string _GAME_SCENE = "Game";
+
+        private SocketIOComponent _Socket;
+
+        void Start()
+        {
+            _Socket = GetComponent<SocketIOComponent>();
+
+            _Socket.On("lobby", OnRoomListUpdated);
+
+            StartCoroutine("SendRefreshRequest");
+        }
+
+        private IEnumerator SendRefreshRequest()
+        {
+            yield return new WaitForSeconds(1);
+            _Socket.Emit("refreshLobby");
+        }
+
+        private void OnRoomListUpdated(SocketIOEvent e)
+        {
+            Debug.Log("[SocketIO] received a message: " + e.name);
+            var roomList = JsonConvert.DeserializeObject<LobbyGames>(e.data.ToString());
+            foreach (var room in roomList.Games)
+            {
+                Debug.Log(room);
+            }
+        }
+
+        public void OnJoinButtonClick()
+        {
+            SceneManager.LoadSceneAsync(_GAME_SCENE);
+        }
+    }
+
 }
