ed105ae589301634cd23171550f778b5a1326b1b
Theo Penavaire <theo.penavaire@gmail.com>
2019-02-15 00:01:51 -0500

    Connect to the correct room through server

--- Audience App/Assets/Scripts/Lobby/LobbyManager.cs
+++ Audience App/Assets/Scripts/Lobby/LobbyManager.cs
@@ -1,53 +1,101 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using audience.lobby.messages;
 using SocketIO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Newtonsoft.Json;
+using UnityEngine.UI;
+using WebSocketSharp;
 
 namespace audience.lobby
 {
 
-    public class LobbyGames
-    {
-        public List<string> Games;
-    }
 
     public class LobbyManager : MonoBehaviour
     {
+        [SerializeField]
+        private InputField _RoomPinInputField;
+
+        #region Private Attributes
+
         private const string _GAME_SCENE = "Game";
 
         private SocketIOComponent _Socket;
+
+        #endregion
+
+        #region Unity API
 
         void Start()
         {
             _Socket = GetComponent<SocketIOComponent>();
 
-            _Socket.On("lobby", OnRoomListUpdated);
+            //_Socket.On(messages.Command.ROOM_LIST, OnRoomListUpdated);
+            _Socket.On(messages.Command.MESSAGE, OnMessage);
 
             StartCoroutine("SendRefreshRequest");
         }
 
+        #endregion
+
+        #region Custom Methods
+
+        private void OnMessage(SocketIOEvent e)
+        {
+            var content = JsonConvert.DeserializeObject<Base>(e.data.ToString());
+            if ((int)content.Code % 10 == 0) // Success codes always have their unit number equal to 0 (cf. protocol)
+            {
+                Debug.Log(content.Content);
+                switch (content.Code)
+                {
+                    case Code.join_game_success:
+                        SceneManager.LoadSceneAsync(_GAME_SCENE);
+                        break;
+                    default: break;
+                }
+            }
+            else
+            {
+                Debug.LogError(content.Content);
+            }
+        } 
+
         private IEnumerator SendRefreshRequest()
         {
             yield return new WaitForSeconds(1);
-            _Socket.Emit("refreshLobby");
+            _Socket.Emit(messages.Command.REFRESH_LOBBY);
         }
 
-        private void OnRoomListUpdated(SocketIOEvent e)
-        {
-            Debug.Log("[SocketIO] received a message: " + e.name);
-            var roomList = JsonConvert.DeserializeObject<LobbyGames>(e.data.ToString());
-            foreach (var room in roomList.Games)
-            {
-                Debug.Log(room);
-            }
-        }
+        // Remove later on if considered useless
+        //private void OnRoomListUpdated(SocketIOEvent e)
+        //{
+        //    Debug.Log("[SocketIO] received a message: " + e.name);
+        //    var roomList = JsonConvert.DeserializeObject<LobbyGames>(e.data.ToString());
+        //    foreach (var room in roomList.Games)
+        //    {
+        //        Debug.Log(room);
+        //        var instance = Instantiate(_SelectRoomButtonPrefab, _ScrollView.transform);
+        //        var button = instance.GetComponent<Button>();
+        //        button.GetComponentInChildren<Text>().text = room;
+        //    }
+        //}
 
         public void OnJoinButtonClick()
         {
-            SceneManager.LoadSceneAsync(_GAME_SCENE);
+            if (!_RoomPinInputField.text.IsNullOrEmpty())
+            {
+                var asInt = int.Parse(_RoomPinInputField.text);
+                var serialized = JsonConvert.SerializeObject(asInt);
+                _Socket.Emit(messages.Command.JOIN_GAME, new JSONObject(serialized));
+            }
+            else
+            {
+                Debug.LogError("Please enter a room PIN");
+            }
         }
+
+        #endregion
     }
 
 }
---
+++ Audience App/Assets/Scripts/Lobby/SocketIoMessages.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace audience.lobby.messages
+{
+    public class Command
+    {
+        // Requests
+        public const string JOIN_GAME = "joinGame";
+        public const string REFRESH_LOBBY = "refreshLobby";
+
+        // Responses
+        public const string MESSAGE = "message";
+        public const string ROOM_LIST = "roomList";
+    }
+
+    public enum Code
+    {
+        join_game_success = 240,
+        join_game_error = 241,
+
+        refresh_lobby_error = 251,
+    }
+
+    public class Base
+    {
+        public User User;
+        public Code Code;
+        public string Content;
+    }
+
+    public class User
+    {
+        public string Name;
+    }
+
+    public class LobbyGames
+    {
+        public List<string> Games;
+    }
+}
