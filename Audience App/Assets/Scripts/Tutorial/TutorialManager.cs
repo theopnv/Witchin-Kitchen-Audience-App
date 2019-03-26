9e359d4cdf173631847943bfa1d10835c6b233ed
Theo Penavaire <theo.penavaire@gmail.com>
2019-03-26 18:54:26 -0400

    Receive EndGame events from Tutorial

--- Audience App/Assets/Scripts/Tutorial/TutorialManager.cs
+++ Audience App/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -26,12 +26,14 @@ namespace audience.tutorial
             _NetworkManager = FindObjectOfType<NetworkManager>();
             _NetworkManager.OnReceivedStopIngredientPoll += OnReceivedStopIngredientPoll;
             _NetworkManager.OnDisconnected += OnDisconnectedFromServer;
+            _NetworkManager.OnReceivedEndGame += OnReceiveEndGame;
         }
 
         void OnDisable()
         {
             _NetworkManager.OnReceivedStopIngredientPoll -= OnReceivedStopIngredientPoll;
             _NetworkManager.OnDisconnected -= OnDisconnectedFromServer;
+            _NetworkManager.OnReceivedEndGame -= OnReceiveEndGame;
         }
 
         void OnDisconnectedFromServer()
@@ -52,6 +54,26 @@ namespace audience.tutorial
         void OnReceivedStopIngredientPoll()
         {
             TransmitIngredientPoll.WasAskedToVote = false;
+        }
+
+        void OnReceiveEndGame(EndGame endGame)
+        {
+            if (endGame.doRematch)
+            {
+                for (var i = 0; i < GameInfo.PlayerNumber; i++)
+                {
+                    GameInfo.PlayerPotions[i] = 0;
+                }
+
+                SceneManager.LoadSceneAsync(SceneNames.Game);
+            }
+            else
+            {
+                var instance = Instantiate(_OneChoiceOverlayPrefab, _Canvas.transform);
+                var errorOverlay = instance.GetComponent<Overlay>();
+                errorOverlay.Description = "Roll credits! Witchin' Kitchen will be back after these messages!";
+                errorOverlay.Primary += Exit;
+            }
         }
 
         #endregion
